6389bc64d2286f513309de1a767f34616f31abdf
Kenko250 <adefilakehinde001@gmail.com>
2022-10-16 14:05:00 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -Programming is like building a multilingual puzzle
+ *
+ * Return -  Always  return 0
+ */
+
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzl");
+
+	return (0);
+}
+
