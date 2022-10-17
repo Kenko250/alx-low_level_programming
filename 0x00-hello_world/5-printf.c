7d4a92262eeee6d58f9f245a50f36227f75d0d3b
Kenko250 <adefilakehinde001@gmail.com>
2022-10-17 05:09:55 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * Description: main - print string to stdout.
- * Return: Always return 0;
+ * Description: main - print a string to stdout.
+ * Return: 0 if success.
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
