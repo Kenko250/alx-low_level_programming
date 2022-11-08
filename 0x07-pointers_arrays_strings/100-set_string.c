ef1c0d0610cd94b88af30af9b3dfe1803e0dfd1d
Kenko250 <adefilakehinde001@gmail.com>
2022-11-08 13:29:21 -0800

    main.h

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * set_string - sum of the two diagonals
- * @s: ponter1 adress
- * @to: ponter2 value
+ * set_string - a function that sets the value of a pointer to a char
+ * @s: source adress.
+ * @to: target adress.
+ * Return: no return.
  */
 void set_string(char **s, char *to)
 {
